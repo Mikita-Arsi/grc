7d20e22b7ba0e44d2bc3a385861a5db6e2c2e1f4
Mikita-Arsi <ardonnik@gmail.com>
2024-05-22 12:24:25 +0300

    edit

--- bot/routers/event_creator/keyboards.py
+++ bot/routers/event_creator/keyboards.py
@@ -3,14 +3,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def save_keyboard(edit: str, next: str = None, back: str = None, is_save: bool = None):
     builder = InlineKeyboardBuilder()
+    sizes = [1]
     if back:
+        sizes[0] += 1
         builder.button(text='Назад', callback_data=f'event_creator:{back}:back')
     builder.button(text='Изменить', callback_data=f'event_creator:{edit}:edit')
     if next:
+        sizes[0] += 1
         builder.button(text='Продолжить', callback_data=f'event_creator:{next}:next')
     if is_save:
+        sizes.append(1)
         builder.button(text='Сохранить', callback_data='event_creator:save')
-    builder.adjust(2, 1)
+    sizes.append(1)
+    builder.button(text='Удалить черновик', callback_data=f'event_creator:{edit}:next')
+    builder.adjust(*sizes)
     return builder.as_markup()
 
 

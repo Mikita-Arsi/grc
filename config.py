4a8f01fe50b785cc1640098f3e9852b82c6be321
Mikita-Arsi <ardonnik@gmail.com>
2024-07-22 16:03:44 +0300

    for example

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
 
 
-is_deploy = 1
-
+is_deploy = os.getenv("IS_DEPLOY")
 if is_deploy:
     token = os.getenv("TOKEN")
-    webhook_url = 'https://bot-wsd-li-33.amvera.io'
+    webhook_url = ''
 else:
     token = os.getenv("TOKEN")
-    webhook_url = 'https://15be-46-242-8-167.ngrok-free.app'
+    webhook_url = ''
 
 
-DB = "postgresql://postgres:5432@176.119.147.78:5432/bot"
+DB = os.getenv("DB")
